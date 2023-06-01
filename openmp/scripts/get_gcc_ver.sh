d25cb0daf4de27df0f24f1b28bd4cc04f734d184
Reto Achermann <achreto@gmail.com>
2023-06-01 13:54:32 -0700

    make the gcc version supported
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- openmp/scripts/get_gcc_ver.sh
+++ openmp/scripts/get_gcc_ver.sh
@@ -6,8 +6,10 @@ if [ -z $1 ] ; then
 	exit 1;
 fi
 
-gcc_ver=$($1 -v 2>&1 >/dev/null|grep -Po  '(?<=gcc version\s)([0-9]\.[0-9])');
+gcc_ver=$($1 -v 2>&1 | grep -Po  '(?<=gcc version\s)([0-9]+\.[0-9]+)');
+
 gcc_ver_check=$(echo $gcc_ver | cut -d '.' -f 1)
+
 if [ $(( $gcc_ver_check )) -eq 5 ] ; then
 	gcc_branch='gcc-5-branch';
 	echo $gcc_branch;
@@ -23,6 +25,12 @@ elif [ $(( $gcc_ver_check )) -eq 8 ] ; then
 	echo $gcc_branch;
 elif [ $(( $gcc_ver_check )) -eq 9 ] ; then
 	gcc_branch="releases/gcc-9";
+	echo $gcc_branch;
+elif [ $(( $gcc_ver_check )) -eq 10 ] ; then
+	gcc_branch="releases/gcc-10";
+	echo $gcc_branch;
+elif [ $(( $gcc_ver_check )) -eq 11 ] ; then
+	gcc_branch="releases/gcc-11";
 	echo $gcc_branch;
 else
 	echo "Error: Installed GCC version is not supported by the script!";
